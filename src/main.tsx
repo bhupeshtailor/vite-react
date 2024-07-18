3260d0dd1589efe0ff73c8cb19d3d4ab27b48e5a
bhupeshtailor <tailor.bhupesh@gmail.com>
2024-07-18 16:46:00 +0530

    remove gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Forms from "./pages/Forms";
 
-// import Router from './Router';
+import Router from "./Router";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container!);
 root.render(
   <React.StrictMode>
-    <Forms />
+    <Router />
   </React.StrictMode>
 );
